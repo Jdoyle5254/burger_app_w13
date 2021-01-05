a68ab79c8839334a1bc6e0150153bd7ff7ccd02e
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-01-05 14:50:08 -0600

    created sql schema and connection

---
+++ connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const util = require('util'); 
+
+
+const connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "Password1",
+      database: "burger_db"
+    });
+    
+    //connection.connect(); 
+    connection.query=util.promisify(connection.query); 
+
+   connection.connect(function(err) {
+    if (err) throw err;
+      console.log("error")
+  
+  });
+  
+   module.exports = connection 
